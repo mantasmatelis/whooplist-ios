c3c34bb09b6cd24480aebc37e8138469afd76167
Dev Chakraborty <dev@Devs-Mac-mini.local>
2013-12-11 20:43:18 -0500

    Rename.

---
+++ whooplist-ios/whooplist/WLViewController.h
@@ -0,0 +1,15 @@
+//
+//  WLViewController.h
+//  whooplist
+//
+//  Created by Dev Chakraborty on 12/6/2013.
+//  Copyright (c) 2013 Whooplist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLViewController : UIViewController <UITextFieldDelegate>
+
+@property (nonatomic, strong) IBOutlet UIView *resizeView;
+
+@end
---
+++ whooplist-ios/whooplist/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  whooplist
+//
+//  Created by Dev Chakraborty on 12/6/2013.
+//  Copyright (c) 2013 Whooplist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WLAppDelegate class]));
+    }
+}
